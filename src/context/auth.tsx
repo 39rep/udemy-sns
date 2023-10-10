1927877a0235f02aa6e510d5d9b680d6d7ceb01d
39rep <okome3317@gmail.com>
2023-10-11 05:56:11 +0900

    tukareta

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import apiClient from "@/lib/apiClient";
-import React, { ReactNode, useContext, useEffect } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
 interface AuthContextType {
+  user: null | {
+    id: number;
+    email: string;
+    username: string;
+  };
   login: (token: string) => void;
   logout: () => void;
 }
 
 const AuthContext = React.createContext<AuthContextType>({
+  user: null,
   login: () => {},
   logout: () => {},
 });
@@ -22,20 +28,40 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<null | {
+    id: number;
+    email: string;
+    username: string;
+  }>(null);
+
   useEffect(() => {
     const token = localStorage.getItem("auth_token");
-    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+    if (token) {
+      apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+      apiClient
+        .get("/users/find")
+        .then((res) => {
+          setUser(res.data.user);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   }, []);
 
   const login = async (token: string) => {
     localStorage.setItem("auth_token", token);
+    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
   };
 
   const logout = () => {
     localStorage.removeItem("auth_token");
+    delete apiClient.defaults.headers["Authorization"];
+    setUser(null);
   };
 
   const value = {
+    user,
     login,
     logout,
   };
