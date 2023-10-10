353673da7df85d30374686d4d5fe0a24e250af4a
39rep <okome3317@gmail.com>
2023-10-10 10:24:33 +0900

    second commit

---
+++ src/context/auth.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import apiClient from "@/lib/apiClient";
+import React, { ReactNode, useContext, useEffect } from "react";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface AuthContextType {
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = React.createContext<AuthContextType>({
+  login: () => {},
+  logout: () => {},
+});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  useEffect(() => {
+    const token = localStorage.getItem("auth_token");
+    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+  }, []);
+
+  const login = async (token: string) => {
+    localStorage.setItem("auth_token", token);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("auth_token");
+  };
+
+  const value = {
+    login,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
